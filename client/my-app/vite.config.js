7dc814eb68efe27a89bd3a38e2fba918426eb6fa
Ben Cavins <ben.cavins@flatironschool.com>
2024-07-18 16:04:07 -0400

    fix url;

--- client/my-app/vite.config.js
+++ client/my-app/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    '/api': 'http://localhost:5555'
+  }
 })
